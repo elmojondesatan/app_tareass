c85a6bc6260e1fce31574cc0f05c3be8e573d193
Diego Villatoro <davillatoro@scl.edu.gt>
2025-02-04 15:45:43 -0600

    header actualizado

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -1,6 +1,17 @@
 function header(){
     let header = document.createElement('header');
     header.className = "header";
+
+   let logo = document.createElement('img');
+   logo.src = "https://img.freepik.com/vector-gratis/plantilla-logotipo-computadora-tecnologia_23-2149204144.jpg";
+   logo.alt = "";
+   header.appendChild(logo);
+
+
+   let h1 = document.createElement('h1');
+   h1.innerText = "Ready";
+   header.appendChild(h1);
+
     return header;
 }
 
